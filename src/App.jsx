2241265dee8f51becb7a46dba86c3d7fe40ed6fb
codeldu <huseynagha.hagverdiyev@code.edu.az>
2024-05-15 12:14:15 +0400

    bootstrap responsive

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <div className="row align-items-center">
+          <div className="col-lg-6 col-sm-12">
+            <div className='left'>
+              <img src="https://softivuspro.com/accupay/main/assets/images/solution_illustrations.png" alt="#" />
+            </div>
+          </div>
+          <div className="col-lg-6 col-sm-12 ">
+            <div className="row align-items-center">
+              <div className="col-lg-12">
+                <h1>
+                  Consolidate Payroll Processing
+                </h1>
+              </div>
+              <div className="col-lg-12">
+                <p>
+                  We have designed a fast and effective payroll system that streamlines your payment process.
+                </p>
+              </div>
+              <div className="col-sm-6 col-12">
+                <div style={{ height: '80px', border: '1px dashed black' }}></div>
+              </div>
+              <div className="col-sm-6 col-12">
+                <div style={{ height: '80px', border: '1px dashed black' }}></div>
+              </div>
+              <div className="col-sm-6 col-12">
+                <div style={{ height: '80px', border: '1px dashed black' }}></div>
+              </div>
+              <div className="col-sm-6 col-12">
+                <div style={{ height: '80px', border: '1px dashed black' }}></div>
+              </div>
+              <div className="col-4">
+                <button style={{ width: '100%' }}>
+                  Contact us
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3 col-md-6">
+            <div style={{ height: '80px', border: '1px dashed black' }}></div>
+          </div>
+          <div className="col-lg-3 col-md-6">
+            <div style={{ height: '80px', border: '1px dashed black' }}></div>
+          </div>
+          <div className="col-lg-3 col-md-6">
+            <div style={{ height: '80px', border: '1px dashed black' }}></div>
+          </div>
+          <div className="col-lg-3 col-md-6">
+            <div style={{ height: '80px', border: '1px dashed black' }}></div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
